db1cd79966e7a5bd96bf06b30835f328cbfa8d14
rajeswar-tammisetti <rajeswartammisetti@gmail.com>
2026-01-01 15:21:20 +0000

    hi

---
+++ index.js
@@ -0,0 +1,9 @@
+const btn=document.getElementById("mysubmit");
+const mycheckbox=document.getElementById("mycheckbox");
+
+
+btn.onclick=function(){
+  let a='1234567890'
+  console.log(a.padStart(15,"k"));
+}
+
